a107afd058dfd3fdea875d44a2e9a07b2ea7c589
Ricardo-Ballinas <rica2703@gmail.com>
2023-10-05 21:50:43 -0600

    tercer commit

---
+++ ejemplo.jsx
@@ -0,0 +1,39 @@
+// Importa el módulo mysql2
+const mysql = require('mysql2');
+
+// Configuración de la conexión a la base de datos
+const connection = mysql.createConnection({
+  host: 'localhost', // Cambia esto a la dirección de tu servidor MySQL
+  user: 'tu_usuario',
+  password: 'tu_contraseña',
+  database: 'tu_base_de_datos'
+});
+
+// Conectar a la base de datos
+connection.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos: ' + err.stack);
+    return;
+  }
+  console.log('Conexión exitosa a la base de datos MySQL');
+});
+
+// Ejemplo de consulta a la base de datos
+connection.query('SELECT * FROM usuarios', (err, results, fields) => {
+  if (err) {
+    console.error('Error al ejecutar la consulta: ' + err.message);
+    return;
+  }
+  console.log('Resultados de la consulta:');
+  console.log(results);
+
+  // Puedes realizar más operaciones aquí
+
+  // Cierra la conexión a la base de datos al finalizar
+  connection.end((err) => {
+    if (err) {
+      console.error('Error al cerrar la conexión: ' + err.message);
+    }
+    console.log('Conexión a la base de datos cerrada');
+  });
+});
